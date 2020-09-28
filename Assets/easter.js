16cdf7da17ca88703bdee61690de8715c7e76e5b
Tonva07 <teva@2gik.ch>
2020-09-28 14:49:26 +0200

    ff

--- Assets/easter.js
+++ Assets/easter.js
@@ -1,8 +1,8 @@
 // Display or Hide the easter egg
 $('#easter_open').on("click", function(){
-    $('#hidden_section').addClass('isVisible');
+    $('#hidden_section').removeClass('isHidden');
 });
 
 $('#easter_close').on("click", function(){
-    $('#hidden_section').removeClass('isVisible');
+    $('#hidden_section').addClass('isHidden');
 });
