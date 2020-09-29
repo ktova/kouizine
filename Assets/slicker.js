19cfe0d8f26b3a2fc5e48874e4b706e0b47c3b69
Tonva07 <teva@2gik.ch>
2020-09-29 09:24:55 +0200

    Update slicker.js

--- Assets/slicker.js
+++ Assets/slicker.js
@@ -1,7 +1,25 @@
 // Init slickjs 
 
-$(document).ready(function(){
-    $('.category_slider').slick({
-        //
-    });
-});
+$('.category_slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
