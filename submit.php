a0a27c5caffef807b321757926a631f1c20e90fc
Tonva07 <teva@2gik.ch>
2020-10-05 12:26:30 +0200

    php headers

--- submit.php
+++ submit.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon site de kouizine</title>
-    <link rel="icon" href="/Assets/favicon.png"  type="image/icon type">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="/Assets/styles.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/0d2059c859.js" crossorigin="anonymous"></script>
-</head>
+<?php
+// Load header
+require_once('templates/header.html');
+?>
 
-<main class="container">
+<!-- Site content -->
 
     <header>
         <a class="d-flex justify-content-center" href="/home.html">
