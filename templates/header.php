c616fdabf2f9566961154c405c0a2273fb0987c5
Tonva07 <teva@2gik.ch>
2020-10-05 14:11:29 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,7 @@
     <?php }
     ?>
     <link rel="stylesheet" href="/Assets/styles.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/0d2059c859.js" crossorigin="anonymous"></script>
 </head>
 
